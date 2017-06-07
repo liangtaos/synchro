7db61739596782166c475bbc5da22dc5237e6b3b
lt <lt541977844@163.com>
2017-06-07 22:28:19 +0800

    tieba

--- tieba/he.py
+++ tieba/he.py
@@ -12,10 +12,16 @@ def getHtml(url):
 def htmlRe(html):
     #li = re.findall(r'class="BDE_Image" src="(.*?)" size',html)
     li = re.findall(r'img class="BDE_Image" src=(.*?) size',html)
-    print li
+    for url_pic in li:
+        url_pic = url_pic.strip()
+	url_pic = urllib.unquote(url_pic).encode('utf-8')
+        print url_pic
+        print urllib2.urlopen(url_pic).read()
+        #urllib.urlretrieve(url_pic)
 def main():
     url = 'http://tieba.baidu.com/p/5141114266'
     html = getHtml(url)
+    htmlRe(html)
 
 if __name__ == '__main__':
     main()
